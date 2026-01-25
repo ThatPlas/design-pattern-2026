96d6c4dddaf25e0fd8147807862fe55e2ed3486c
kappacite <robyn.denoeud@lacatholille.fr>
2026-01-25 19:31:13 +0100

    Removed comment

--- src/main/java/fr/fges/Main.java
+++ src/main/java/fr/fges/Main.java
@@ -33,36 +33,3 @@ public class Main {
 }
 
 
-
-
-// package fr.fges;
-
-// import fr.fges.game.GameRepository;
-// import fr.fges.ExtensionChecker;
-// import fr.fges.Menu;
-
-// import java.util.List;
-
-// public class Main {
-//     public static void main(String[] args) {
-//         if (args.length < 1) {
-//             System.out.println("Usage: java -jar boardgamecollection.jar <storage-file>");
-//             System.out.println("Storage file must be .json or .csv");
-//             System.exit(1);
-//         }
-
-//         String storageFile = args[0];
-
-//         ExtensionChecker.checkFileExtension(storageFile);
-
-//         GameRepository.loadFromFile(storageFile);
-
-//         System.out.println("Using storage file: " + storageFile);
-
-//         while (true) {
-//             Menu.handleMenu();
-//         }
-//     }
-// }
-
-
