22987a150d921570646facda6e1ece8ea9188a88
EythanTournant <eythan.tournant@gmail.com>
2026-02-08 13:39:31 +0100

    Tests pour la nouvelle feature

--- src/test/java/fr/fges/GameServiceTest.java
+++ src/test/java/fr/fges/GameServiceTest.java
@@ -1,8 +1,11 @@
 package fr.fges;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import fr.fges.game.GameCollection;
 import fr.fges.game.GameService;
@@ -42,5 +45,47 @@ class GameServiceTest {
         GameService.removeGame(collection);
 
         assertEquals(0, collection.getGames().size());
+    }
+
+    @Test
+    void filterGames_shouldBeEmpty() {
+        GameCollection collection = new GameCollection();
+        BoardGame game = new BoardGame("Catan", 3, 4, "Strategy");
+        collection.addGame(game);
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        PrintStream original = System.out;
+        System.setOut(new PrintStream(out));
+
+        InputHandler.setInput(new ByteArrayInputStream("2\n".getBytes()));
+        try {
+            GameService.filterGamesByPlayerCount(collection);
+        } finally {
+            System.setOut(original);
+        }
+        String output = out.toString();
+
+        assertTrue(output.contains("No games found for 2 player(s)."));
+    }
+
+    @Test
+    void filterGames_shouldReturnOne() {
+        GameCollection collection = new GameCollection();
+        BoardGame game = new BoardGame("Dead Cells", 2, 4, "Strategy");
+        collection.addGame(game);
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        PrintStream original = System.out;
+        System.setOut(new PrintStream(out));
+
+        InputHandler.setInput(new ByteArrayInputStream("2\n".getBytes()));
+        try {
+            GameService.filterGamesByPlayerCount(collection);
+        } finally {
+            System.setOut(original);
+        }
+        String output = out.toString();
+
+        assertTrue(output.contains("=== Games for 2 player(s) ==="));
     }
 }
